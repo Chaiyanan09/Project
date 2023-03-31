5ccd208bf0bc5ec9f4b29e981caff69ef44229a7
Chaiyanan09 <chaiyanan.cha@ku.th>
2023-03-31 21:30:15 +0700

    Edit Window1

--- src/Window1.java
+++ src/Window1.java
@@ -32,22 +32,4 @@ public class Window1 {
 
     }
 
-    public void Window2() {
-        frame.setTitle("Thai Cooking Guide");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setSize(800, 800);
-        frame.setVisible(true);
-
-        try {
-            bgicon = new ImageIcon(getClass().getResource("Tumyumkoung.png"));
-            label2 = new JLabel(bgicon);
-            frame.add(label2);
-        } catch (Exception e) {
-            System.out.println("null");
-        }
-        frame.setSize(800, 800);
-        frame.setVisible(true);
-    }
-
 }
